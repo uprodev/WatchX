f0195a43ff6b296bbd904f3bb171a90f50187589
Stanislav-pr <stanislav.retinskiy@gmail.com>
2024-03-12 15:11:56 +0100

    brand

--- theme/watchx/inc/extras.php
+++ theme/watchx/inc/extras.php
@@ -20,3 +20,19 @@ add_filter( 'excerpt_more', function( $more ) {
 
 add_filter('wpcf7_autop_or_not', '__return_false');
 
+/* alphabet brand */
+
+add_filter( 'terms_clauses', 'terms_clauses_47840519', 10, 3 );
+function terms_clauses_47840519( $clauses, $taxonomies, $args ){
+    global $wpdb;
+
+    if( !isset( $args['__first_letter'] ) ){
+        return $clauses;
+    }
+
+    $clauses['where'] .= ' AND ' . $wpdb->prepare( "t.name LIKE %s", $wpdb->esc_like( $args['__first_letter'] ) . '%' );
+
+    return $clauses;
+
+}
+
---
+++ theme/watchx/templates/brand.php
@@ -0,0 +1,100 @@
+<?php
+
+/*
+
+Template Name: Brands
+
+*/
+
+$alpha = range("a","z");
+
+get_header();
+
+$desc = get_field('description');
+$title = get_field('title');
+$text = get_field('text');
+$brands = get_field('brands');
+
+?>
+
+    <section class="category-banner">
+        <div class="content-width">
+            <div class="text-wrap">
+                <h1><?php the_title();?></h1>
+                <?php if($desc):?>
+                    <p><?= $desc;?></p>
+                <?php endif;?>
+                <?php if($desc):?>
+                    <ul class="list-brand">
+                        <?php foreach($brands as $brand):
+                            $img = get_field('thumbnail','pa_brand_'.$brand);
+
+                        ?>
+                            <li>
+                                <a href="<?= get_term_link($brand);?>">
+                                    <figure>
+                                        <img src="<?= $img?$img['url']:get_template_directory_uri().'/img/img-10.png';?>" alt="">
+                                    </figure>
+                                    <div class="text">
+                                        <p><?= get_term( $brand )->name;?></p>
+                                    </div>
+                                </a>
+                            </li>
+                        <?php endforeach;?>
+                    </ul>
+                <?php endif;?>
+            </div>
+        </div>
+    </section>
+
+    <section class="search-brand">
+        <div class="content-width">
+            <div class="title-wrap">
+                <div class="title">
+                    <?php if($title):?>
+                        <h2 class="title-h2"><?= $title;?></h2>
+                    <?php endif;?>
+                    <?php if($text):?>
+                        <p><?= $text;?></p>
+                    <?php endif;?>
+                </div>
+                <div class="form-wrap form-search">
+                    <form action="#">
+                        <label for="search"></label>
+                        <input type="search" name="search" id="search" placeholder="<?= __('Search all brands...', 'watchx');?>">
+                        <button type="submit" class="btn"><img src="<?= get_template_directory_uri();?>/img/icon-search.svg" alt=""></button>
+                    </form>
+                </div>
+            </div>
+            <div class="content">
+                <?php foreach ($alpha as $letr):
+                    $terms = get_terms( array(
+                        'taxonomy' => 'pa_brand',
+                        '__first_letter' => $letr,
+                        'hide_empty' => false
+                    ) );
+
+                    if($terms):?>
+
+                        <div class="item">
+                            <h6 class="title-item"><?= $letr;?></h6>
+                            <ul>
+                                <?php foreach($terms as $term):?>
+
+                                    <li>
+                                        <a href="<?= get_term_link($term->term_id);?>"><?= $term->name;?></a>
+                                    </li>
+
+                                <?php endforeach;?>
+                            </ul>
+                        </div>
+
+                    <?php endif;
+
+                endforeach;?>
+
+            </div>
+        </div>
+    </section>
+
+<?php get_footer();
