cc62375a873576449a25130fed2762315cb58afb
Stanislav-pr <stanislav.retinskiy@gmail.com>
2024-06-06 12:12:19 +0200

    Create text.php

---
+++ theme/watchx/templates/sections/layout/text.php
@@ -0,0 +1,17 @@
+<?php
+
+$text = get_sub_field('text');
+
+?>
+
+<section class="about about-text">
+    <div class="content-width">
+        <div class="content">
+            <div class="main-about">
+
+                <?= $text;?>
+
+            </div>
+        </div>
+    </div>
+</section>
