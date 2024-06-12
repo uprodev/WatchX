5402e6e0865b0cd1b0936b03b84af13ac5770fb7
Stanislav-pr <stanislav.retinskiy@gmail.com>
2024-06-12 13:22:14 +0200

    Update text.php

--- theme/watchx/templates/sections/layout/text.php
+++ theme/watchx/templates/sections/layout/text.php
@@ -4,7 +4,7 @@ $text = get_sub_field('text');
 
 ?>
 
-<section class="about about-text">
+<section class="about about-text about-green">
     <div class="content-width">
         <div class="content">
             <div class="main-about">
